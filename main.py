472d7817e49d390d48fe31326fd36e42aab81a38
Shreya Bhalgat <85868386+shreyabhalgat@users.noreply.github.com>
2023-10-10 19:45:32 +0530

    Added unit conversion feature for temperature (Celsius/Fahrenheit toggl

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def getWeather():
     wind= json_data['current']['wind_speed']
     description = json_data['current']['weather'][0]['description']
 
-    t.config(text=(temp,"°C"))
+    update_temperature_labels(temp)
     h.config(text=(humidity,"%"))
     p.config(text=(pressure,"hPa"))
     w.config(text=(wind,"m/s"))
@@ -143,7 +143,6 @@ def getWeather():
 
     day7temp.config(text=f"Day:{tempday7}\n Night:{tempnight7}")
 
-
     #days
     first = datetime.now()
     day1.config(text=first.strftime("%A"))
@@ -166,185 +165,6 @@ def getWeather():
     seventh = first+timedelta(days=6)
     day7.config(text=seventh.strftime("%A"))
 
-
-##icon
-image_icon=PhotoImage(file="Images\logo.png")
-root.iconphoto(False,image_icon)
-
-Round_box=PhotoImage(file="Images\Rounded Rectangle 1.png")
-Label(root,image=Round_box,bg="#57adff").place(x=30,y=110)
-
-#label
-label1=Label(root,text="Temperature",font=('Helvetica',11),fg='white',bg='#203243')
-label1.place(x=50,y=120)
-
-label2=Label(root,text="Humidity",font=('Helvetica',11),fg='white',bg='#203243')
-label2.place(x=50,y=140)
-
-label3=Label(root,text="Pressure",font=('Helvetica',11),fg='white',bg='#203243')
-label3.place(x=50,y=160)
-
-label4=Label(root,text="Wind Speed",font=('Helvetica',11),fg='white',bg='#203243')
-label4.place(x=50,y=180)
-
-label5=Label(root,text="Description",font=('Helvetica',11),fg='white',bg='#203243')
-label5.place(x=50,y=200)
-
-##logo
-logo = Image.open("Copy of logo.png")
-resize_logo = logo.resize((100,100))
-img = ImageTk.PhotoImage(resize_logo)
-mylogoimage = Label(root,image=img,bg="#57adff")
-mylogoimage.pack()
-
-##search box
-Search_image=PhotoImage(file="Images\Rounded Rectangle 3.png")
-myimage=Label(image=Search_image,bg="#57adff")
-myimage.place(x=270,y=120)
-
-weat_image=PhotoImage(file="Images\Layer 7.png")
-weatherimage= Label(root,image=weat_image,bg="#203243")
-weatherimage.place(x=290,y=127)
-
-textfield=tk.Entry(root,justify="center",width=15,font=("poppins",25,"bold"),bg="#203243",border=0,fg="white")
-textfield.place(x=370,y=130)
-textfield.focus()
-
-Search_icon=PhotoImage(file="Images\Layer 6.png")
-myimage_icon=Button(image=Search_icon,borderwidth=0,cursor="hand2",bg="#203243",command=getWeather)
-myimage_icon.place(x=645,y=125)
-
-
-##Bottom Box
-frame= Frame(root,width=900,height=180,bg="#212120")
-frame.pack(side=BOTTOM)
-
-##bottom boxes
-firstbox = PhotoImage(file="Images\Rounded Rectangle 2.png")
-secondbox = PhotoImage(file="Images\Rounded Rectangle 2 copy.png")
-
-Label(frame,image=firstbox,bg="#212120").place(x=30,y=20)
-Label(frame,image=secondbox,bg="#212120").place(x=300,y=30)
-Label(frame,image=secondbox,bg="#212120").place(x=400,y=30)
-Label(frame,image=secondbox,bg="#212120").place(x=500,y=30)
-Label(frame,image=secondbox,bg="#212120").place(x=600,y=30)
-Label(frame,image=secondbox,bg="#212120").place(x=700,y=30)
-Label(frame,image=secondbox,bg="#212120").place(x=800,y=30)
-
-##clock(time)
-clock=Label(root,font=("Helvetica",30,"bold"),fg="white",bg="#57adff")
-clock.place(x=30,y=20)
-
-#timezone
-Timezone=Label(root,font=("Helvetica",20,"bold"),fg="white",bg="#57adff")
-Timezone.place(x=650,y=20)
-
-Long_lat=Label(root,font=("Helvetica",15,"bold"),fg="white",bg="#57adff")
-Long_lat.place(x=650,y=50)
-
-##thpwd
-t= Label(root,text="...",font=("Helvetica",11),fg="white",bg="#203243")
-t.place(x=150,y=120)
-h= Label(root,text="...",font=("Helvetica",11),fg="white",bg="#203243")
-h.place(x=150,y=140)
-p= Label(root,text="...",font=("Helvetica",11),fg="white",bg="#203243")
-p.place(x=150,y=160)
-w= Label(root,text="...",font=("Helvetica",11),fg="white",bg="#203243")
-w.place(x=150,y=180)
-d= Label(root,text="...",font=("Helvetica",11),fg="white",bg="#203243")
-d.place(x=150,y=200)
-
-
-##first cell
-firstframe = Frame(root,width=230,height=132,bg="#282829")
-firstframe.place(x=35,y=315)
-
-firstimage = Label(firstframe,bg="#282829")
-firstimage.place(x=1,y=15)
-
-day1 = Label(firstframe,font="arial 20",bg="#282829",fg="#fff")
-day1.place(x=20,y=5)
-
-day1temp = Label(firstframe,bg="#282829",fg="#57adff",font="arial 15 bold")
-day1temp.place(x=100,y=50)
-
-##second cell
-secondframe = Frame(root,width=70,height=115,bg="#282829")
-secondframe.place(x=305,y=325)
-
-secondimage =Label(secondframe,bg="#282829")
-secondimage.place(x=7,y=20)
-
-day2 = Label(secondframe,bg="#282829",fg="#fff")
-day2.place(x=2,y=5)
-
-day2temp= Label(secondframe,bg="#282829",fg="#fff")
-day2temp.place(x=10,y=70)
-
-#third cell
-thirdframe = Frame(root,width=70,height=115,bg="#282829")
-thirdframe.place(x=405,y=325)
-
-thirdimage =Label(thirdframe,bg="#282829")
-thirdimage.place(x=7,y=20)
-
-day3 = Label(thirdframe,bg="#282829",fg="#fff")
-day3.place(x=2,y=5)
-
-day3temp= Label(thirdframe,bg="#282829",fg="#fff")
-day3temp.place(x=10,y=70)
-
-##fourth cell
-fourthframe = Frame(root,width=70,height=115,bg="#282829")
-fourthframe.place(x=505,y=325)
-
-fourthimage =Label(fourthframe,bg="#282829")
-fourthimage.place(x=7,y=20)
-
-day4 = Label(fourthframe,bg="#282829",fg="#fff")
-day4.place(x=2,y=5)
-
-day4temp= Label(fourthframe,bg="#282829",fg="#fff")
-day4temp.place(x=10,y=70)
-
-##fifth cell
-fifthframe = Frame(root,width=70,height=115,bg="#282829")
-fifthframe.place(x=605,y=325)
-
-fifthimage =Label(fifthframe,bg="#282829")
-fifthimage.place(x=7,y=20)
-
-day5 = Label(fifthframe,bg="#282829",fg="#fff")
-day5.place(x=2,y=5)
-
-day5temp= Label(fifthframe,bg="#282829",fg="#fff")
-day5temp.place(x=10,y=70)
-
-##sixth cell
-sixthframe = Frame(root,width=70,height=115,bg="#282829")
-sixthframe.place(x=705,y=325)
-
-sixthimage =Label(sixthframe,bg="#282829")
-sixthimage.place(x=7,y=20)
-
-day6 = Label(sixthframe,bg="#282829",fg="#fff")
-day6.place(x=2,y=5)
-
-day6temp= Label(sixthframe,bg="#282829",fg="#fff")
-day6temp.place(x=10,y=70)
-
-##seventh cell
-seventhframe = Frame(root,width=70,height=115,bg="#282829")
-seventhframe.place(x=805,y=325)
-
-seventhimage =Label(seventhframe,bg="#282829")
-seventhimage.place(x=7,y=20)
-
-day7 = Label(seventhframe,bg="#282829",fg="#fff")
-day7.place(x=2,y=5)
-
-day7temp= Label(seventhframe,bg="#282829",fg="#fff")
-day7temp.place(x=10,y=70)
-
-
-root.mainloop()
+## Add unit conversion button
+unit_button = Button(root, text="Toggle Unit", command=toggle_unit)
+unit_button
